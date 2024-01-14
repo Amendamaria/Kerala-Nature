093937e53f4138dad85e6797347159c0b42a60d3
Amenda <144139197+Amendamaria@users.noreply.github.com>
2024-01-14 08:42:53 +0530

    update

--- load/loadsc.js
+++ load/loadsc.js
@@ -2,6 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
     // Simulate content loading delay (remove this in a real scenario)
     setTimeout(function() {
         document.getElementById('loadingContainer').style.display = 'none';
-        document.getElementById('content').style.display = 'block';
     }, 2000); // Adjust the delay time as needed
 });
